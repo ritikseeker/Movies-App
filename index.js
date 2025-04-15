0ecd78dec6468828b2ffe4d5ef1ff57ec1095ec6
Ritik <ritikraturiworks@gmail.com>
2025-04-15 17:15:20 +0000

    project initiated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import ReviewsDAO from "./dao/reviewsDAO.js"
 const MongoClient = mongodb.MongoClient
 const mongo_username = process.env['MONGO_USERNAME']
 const mongo_password = process.env['MONGO_PASSWORD']
-const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.ayr9czp.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.eo5nl4i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 const port = 8000
 
